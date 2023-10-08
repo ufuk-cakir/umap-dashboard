cda625c762d18bc5f1e98cd0cadf480c7712e374
Ufuk Çakir <ufukcakir2001@gmail.com>
2023-10-08 14:53:09 +0200

    new images

--- app.py
+++ app.py
@@ -13,27 +13,27 @@ from PIL import Image
 
 PATH = pathlib.Path(__file__).parent
 DATA_PATH= PATH.joinpath("data").resolve()
-IMAGE_PATH = DATA_PATH.joinpath("plotly_images")
+IMAGE_PATH = DATA_PATH.joinpath("masses")
 
 
-df = np.load(DATA_PATH.joinpath("umap_data.npy"), allow_pickle=True).item()
+df = np.load(DATA_PATH.joinpath("umap_embedding.npy"))
 
-x_dat = df["umap"][:,0]
-y_dat = df["umap"][:,1]
+
+log_masses = np.load(DATA_PATH.joinpath("log_masses.npy"))
+halo_ids = np.load(DATA_PATH.joinpath("halo_ids.npy"))
+x_dat = df[:,0]
+y_dat = df[:,1]
 
 
 size_norm = 600
-color_bar = "Mass"
-
-size = size_norm*df["p_late"]
+color_bar = "log10(M/M_sun)"
 
 
-masses = df["Mass"]
-masses = masses - masses.min()
-masses = masses/masses.max()
 
+masses = log_masses
+color = masses.copy()
+color = (color - color.min())/color.max()
 color =masses
-
 fig = go.Figure(data=[
     go.Scatter(
         x=x_dat,
@@ -42,7 +42,6 @@ fig = go.Figure(data=[
         marker=dict(
             colorscale="viridis",
             color=color,
-            size=size,
             colorbar={"title": color_bar},
             line={"color": "#444"},
             reversescale=True,
@@ -93,21 +92,20 @@ def display_hover(hoverData):
     bbox = pt["bbox"]
     num = pt["pointNumber"]
 
+    # Get halo_id of corresponding point
+    halo_id = halo_ids[num]
     #df_row = df.iloc[num]
     #img_src = df_row['IMG_URL']
-    img_src = Image.open(f"{IMAGE_PATH}/{str(num).zfill(5)}.png")
+    img_src = Image.open(f"{IMAGE_PATH}/masses_{str(num)}.png")
     #img_src = f"https://ufuk-bachelor-thesis.s3.eu-west-2.amazonaws.com/plotly_images/{str(num).zfill(5)}.png"
-    name = f"TNG ID: {df['halo_ids'][num]}"
-    ellip = f"Late Type Probability: {round(df['p_late'][num],2)}"
-    mass = f"Mass: {round(df['Mass'][num],2)}*10^10 M_sun"
+    name = f"TNG ID: {halo_ids[num]}"
+    
     #if len(desc) > 300: desc = desc[:100] + '...'
     
     children = [
         html.Div(children=[
             html.Img(src=img_src,alt="image", style={"width": "100%"}),
             html.H2(f"{name}", style={"color": "darkblue"}),
-            html.P(f"{ellip}"),
-            html.P(f"{mass}"),
         ],
         style={'width': '200px', 'white-space': 'normal'})
     ]
